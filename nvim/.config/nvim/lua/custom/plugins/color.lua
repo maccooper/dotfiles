5d6f48ad1afc94b93cd579389fed95f0843e9832
Liam Cooper <liamcoop@outlook.com>
2025-05-19 21:04:23 -0700

    updates to use catpuccin-mocha across the board

--- nvim/.config/nvim/lua/custom/plugins/color.lua
+++ nvim/.config/nvim/lua/custom/plugins/color.lua
@@ -3,9 +3,12 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-        require("catppuccin").setup()
+        require("catppuccin").setup({
+            flavour = "mocha", -- Set the theme flavour to 'latte'
+        })
 
         -- setup must be called before loading
         vim.cmd.colorscheme "catppuccin"
     end,
 }
+
