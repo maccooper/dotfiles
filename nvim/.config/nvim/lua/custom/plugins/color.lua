f3db0e4bb58b3ba1b49b68aee7bdacff9c2110db
Liam Cooper <liamcoop@outlook.com>
2025-05-19 20:54:23 -0700

    updated themes to catpuccin-frappe

--- nvim/.config/nvim/lua/custom/plugins/color.lua
+++ nvim/.config/nvim/lua/custom/plugins/color.lua
@@ -1,4 +1,11 @@
 return {
-    "rose-pine/neovim"
-}
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup()
 
+        -- setup must be called before loading
+        vim.cmd.colorscheme "catppuccin"
+    end,
+}
