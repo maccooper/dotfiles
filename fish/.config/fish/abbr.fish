0eb4f5e09cacceccd83f08061318cbfe509f3a48
Liam Cooper <liamcoop@outlook.com>
2025-05-26 08:06:20 -0700

    starship updates, nvim / fish updates, karabiner updates

--- fish/.config/fish/abbr.fish
+++ fish/.config/fish/abbr.fish
@@ -8,8 +8,3 @@ abbr gd git diff --output-indicator-new='' --output-indicator-old=''
 
 abbr find fd
 abbr ev 'nvim $HOME/.config/nvim'
-
-# For config of dotfiles: https://www.atlassian.com/git/tutorials/dotfiles
-# manage symlinks
-abbr config /usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME
-abbr sshserver ssh liamcoop@100.109.140.25 -p 50022
--- fish/.config/fish/alias.fish
+++ fish/.config/fish/alias.fish
@@ -1,6 +1,5 @@
 
 alias find='echo "Using fd instead of find!"; fd'
-alias cat='echo "Using cat instead of bat!"; bat'
 
 alias welcome="-l lca168 -p 3222 welcomeepbc.its.sfu.ca"
 # alias jump="liam@epbc-jump.epbc.sfu.ca"
