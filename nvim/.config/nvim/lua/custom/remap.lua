a7869ab978902b3ddd8be9a08a3f5b85b80c3920
maccooper <Mackenzie.Cooper99@gmail.com>
2026-02-06 19:31:48 -0800

    add ease of copying to clipboard form nvim

--- nvim/.config/nvim/lua/custom/remap.lua
+++ nvim/.config/nvim/lua/custom/remap.lua
@@ -14,3 +14,10 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "vv", "<C-w>v")
 
 vim.keymap.set("n", "<leader>r", "<cmd>edit<CR>", { desc = "Reload file from disk" })
+vim.keymap.set("n", "<leader>wd", function()
+    local path = vim.fn.expand("%:p")
+    if path == "" or vim.fn.isdirectory(path) == 1 then
+        path = vim.fn.getcwd()
+    end
+    vim.fn.setreg("+", path)
+end, { desc = "Copy current file/dir path to clipboard" })
